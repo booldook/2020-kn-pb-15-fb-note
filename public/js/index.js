867e3ab160fd114f4ae983638435fb823f5563b0
booldook <booldook@gmail.com>
2020-06-29 16:46:53 +0900

    login

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 /**************** 전역설정 *****************/
-
+var auth = firebase.auth();
+var googleAuth = new firebase.auth.GoogleAuthProvider();
 
 
 /**************** 사용자 지정 *****************/
@@ -7,7 +8,10 @@
 
 
 /**************** 이벤트 콜백 *****************/
-
+function onLogin() {
+	auth.signInWithPopup(googleAuth);
+}
 
 
 /**************** 이벤트 등록 *****************/
+$(".bt-login").click(onLogin);
