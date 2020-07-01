95a0fba02277190bba3ca7e87c17453c48491acf
booldook <booldook@gmail.com>
2020-07-01 17:13:53 +0900

    만드는 중

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,11 +8,21 @@ var ref = null;
 
 /**************** 사용자지정 *****************/
 function init() {
-	
+	db.ref("root/chats").on("child_added", onAdded);
 }
 
 
 /**************** 이벤트콜백 *****************/
+function onAdded(r) {
+	var html;
+	if(user.uid === r.val().writer_id) html = '<div class="chat p-2 me">';
+	else html = '<div class="chat p-2">';
+	html += '<div class="cont text-light p-3">'+r.val().chat+'</div>';
+	html += '<div class="writer">'+r.val().writer+'</div>';
+	html += '</div>';
+	$(".chat-stage").append(html);
+}
+
 function onAuth(r) {
 	if(r) {	// 로그인 상태
 		$(".bt-login").hide();
@@ -36,7 +46,21 @@ function onLogout() {
 	auth.signOut();
 }
 
+function onSave() {
+	var chat = $("#chat-txt").val().trim();
+	if(chat == "") alert("글을 작성하세요.");
+	else {
+		db.ref("root/chats").push({
+			chat: chat,
+			writer: user.displayName,
+			writer_id: user.uid,
+			createdAt: moment().format('YYYY-MM-DD HH:mm:ss')
+		}).key;
+	}
+}
+
 /**************** 이벤트선언 *****************/
 auth.onAuthStateChanged(onAuth);
 $(".bt-login").click(onLogin);
 $(".bt-logout").click(onLogout);
+$(".bt-save").click(onSave);
